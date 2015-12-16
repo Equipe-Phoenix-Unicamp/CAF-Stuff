58eed01804cfbf403cbc7590605a9e6893e5c830
cmillani <carloseduardomillani@gmail.com>
2015-12-16 15:16:22 -0200

    controller

--- projetoCAF/pins.h
+++ projetoCAF/pins.h
@@ -1,7 +1,17 @@
 #ifndef PIN_H
 #define PIN_H
 
-#define 
+#define cmd           9    // Laranja        
+#define att           11   // Amarelo     
+#define clk           10   // Azul
+#define dado          A0    // Marrom
+
+
+
+
+/*
+	11, 12, e 13 para os sinais MOSI, MISO e SCK, respectivamente. Os pinos CSN e CE serão ligados aos pinos 7 e 8
+*/
 
 
 #endif //PIN_H
