34922586b05bccad19d5e32123ff5d8d15a8c07b
cmillani <carloseduardomillani@gmail.com>
2015-12-16 19:20:43 -0200

    PORTAL PORTOU

--- projetoCAF/projetoCAF.ino
+++ projetoCAF/projetoCAF.ino
@@ -1,58 +1,21 @@
-
-
- #include  <SPI.h>
- #include "nRF24L01.h"
- #include "RF24.h"
- #include "transceptorServices.h"
- #include "hBridgeServices.h"
- #include "controllerServices.h"
+#include  <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "transceptorServices.h"
+#include "hBridgeServices.h"
+#include "controllerServices.h"
 
 int msgg[1];
- void setup()
- {
-	 Serial.begin(9600);
- 	pinMode(A1,OUTPUT);
- 	configurarRadio(RECEPTOR);
- }
- void loop()
- {
-	// Serial.println(radio.available());
-	 while (!radio.read(msgg, 1));
-	 Serial.println(msgg[0]);
-	 // if (radio.available())
-// 	 	{
-// 	 		radio.read(msgg, 1);
-// 	 		Serial.println(msgg[0]);
-// 	 		if (msgg[0] == 111)
-// 	 		{
-// 	 			delay(10);
-// 	 		}
-// 	 		else
-// 	 		{
-//
-// 	 		}
-// 	 		delay(10);
-// 	 	}
-// 	 	else
-// 	 	{
-// 	 		Serial.println("No radio available");
-// 	 	}
- 	// int a = lerPotDir();
-	// Serial.println(a);
-	// delay(10);
-//  	if (a == 2)
-//  	{
-//  		digitalWrite(A1,HIGH);
-//  		delay(1000);
-//  		digitalWrite(A1,LOW);
-//  		delay(1000);
-//  	}
-//  	else
-//  	{
-//  		digitalWrite(A1,HIGH);
-//  		delay(500);
-//  		digitalWrite(A1,LOW);
-//  		delay(500);
-//}
- }
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(A1,OUTPUT);
+	configurarRadio(RECEPTOR);
+}
+void loop()
+{
+	int a = lerPotDir();
+	Serial.println(a);
+	delay(10);
+}
 
--- projetoCAF/transceptorServices.cpp
+++ projetoCAF/transceptorServices.cpp
@@ -1,7 +1,5 @@
 #include "transceptorServices.h"
 
-int msg[1];
-int msgr[1];
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 RF24 radio(9,10);
 
@@ -15,31 +13,26 @@ void configurarRadio(char modo)
     else
     {
     	radio.openReadingPipe(1,pipe);
-		  radio.startListening();
+		radio.startListening();
     }
 }
 void enviarPotencias(int potenciaDireita,int potenciaEsquerda)
 {
 	int velo[2];
-	// msg[0] = 111;
-	velo[0] = potenciaEsquerda;
-	velo[1] = potenciaDireita;
+	velo[1] = potenciaEsquerda;
+	velo[0] = potenciaDireita;
 	radio.write(velo, 2);
 }
 int lerPotDir(void)
 {
 	int velo[2];
-	while(!radio.read( velo, 2 );
+	while(!radio.read( velo, 2 ));
 	return velo[0];
 }
 int lerPotEsq(void)
 {
-	// int velo[2];
-	while ( ! radio.available() );
-	if ( radio.available() )
-    {
-		radio.read( msgr, 1 );
-	}
-	return msgr[0];
+	int velo[2];
+	while(!radio.read( velo, 2 ));
+	return velo[1];
 }
 
