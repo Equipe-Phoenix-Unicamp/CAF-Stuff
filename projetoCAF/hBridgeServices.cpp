cd9a138ff8124088bf81b3af88d5e36c0d03acc5
cmillani <carloseduardomillani@gmail.com>
2015-12-16 15:49:13 -0200

    mode pins and pwm

--- projetoCAF/hBridgeServices.cpp
+++ projetoCAF/hBridgeServices.cpp
@@ -1,6 +1,36 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include "pins.h"
+
+void configuraMotores()
+{
+    pinMode(PWME,INPUT);
+    pinMode(PWMD,INPUT);
+	pinMode(DIRECTIONE,OUTPUT);
+    pinMode(DIRECTIOND,OUTPUT);
+	pinMode(ENABLE,OUTPUT);
+    pinMode(ERROR,INPUT);
+	
+	digitalWrite(ENABLE, HIGH);
+}
+
+void potenciaMotorDireito(int potencia)
+{
+	int direction = potencia>0?HIGH:LOW;
+	int pwm = potencia<0?-potencia:potencia;
+	analogWrite(PWMD,pwm);
+	digitalWrite(DIRECTIONR, direction);
+}
+
+void potenciaMotorEsquerdo(int potencia)
+{
+	int direction = potencia>0?HIGH:LOW;
+	int pwm = potencia<0?-potencia:potencia;
+	analogWrite(PWME,pwm);
+	digitalWrite(DIRECTIONE, direction);
+}
 	
 #ifdef __cplusplus
 }
--- projetoCAF/pins.h
+++ projetoCAF/pins.h
@@ -1,12 +1,35 @@
 #ifndef PIN_H
 #define PIN_H
 
-#define cmd           9    // Laranja        
-#define att           11   // Amarelo     
-#define clk           10   // Azul
-#define dado          A0    // Marrom
+/*
+0 - H
+1 - H
+2 - H
+3 - H
+4 - Contr
+5 - H
+6 - H
+7 - Transcep
+8 - Transcep
+9 - Contr
+10 - Contr
+11 - Transcep
+12 - Transcep
+13 - Transcep
+*/
 
 
+#define cmd	9    // Laranja        
+#define att	4   // Amarelo     
+#define clk	10   // Azul
+#define dado	A0    // Marrom
+
+#define PWME	6
+#define PWMD	5
+#define ENABLE	3
+#define ERROR	2
+#define DIRECTIONE	1
+#define DIRECTIOND	0
 
 
 /*
